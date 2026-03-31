8ad79400e721ccb6e4115116de1fbb4f76837782
JaBabiar <bromomento2137@gmail.com>
2026-03-31 09:57:14 +0200

    Lekcja 1 31.03.2026

---
+++ 1.tptuf.pi/PAI/Tydzien 8/app.js
@@ -0,0 +1,7 @@
+let form = document.getElementById("formLogin");
+
+form.addEventListener("submit", function(e){
+    e.preventDefault();
+    console.log("Formularz został wysłany");
+})
+// https://github.com/JaBabiar/TEB.TOR.25-26.S2
