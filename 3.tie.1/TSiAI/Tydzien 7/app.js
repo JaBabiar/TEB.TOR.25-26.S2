95e4d2cf175289bb2367538d39be896dae1c44b1
Jakub Babiarczyk <bromomento2137@gmail.com>
2026-04-09 11:15:48 +0200

    Dodanie lekcji z 9.04

---
+++ 3.tie.1/TSiAI/Tydzien 7/app.js
@@ -0,0 +1,33 @@
+/*
+document.getElementById("submit").addEventListener("click", function() {
+    if(
+        !document.getElementById("name").value ||
+        !document.getElementById("email").value ||
+        !document.getElementById("pwd").value
+    ){
+        console.error("Błąd Rejestracji")
+        return
+    }
+
+    console.log(
+        document.getElementById("name").value + " " +
+        document.getElementById("email").value + " " +
+        document.getElementById("pwd").value
+    )
+}) */
+let submit = document.getElementById('submit')
+let userName = document.getElementById("name")
+let email = document.getElementById("email")
+let pwd = document.getElementById("pwd")
+submit.addEventListener("click", function(){
+    if(!pwd.value || !userName.value || !email.value){
+        console.error("Błąd Rejestracji")
+        return
+    }
+    console.log(
+        userName.value + " " +
+        email.value + " " +
+        pwd.value 
+    )
+
+})
