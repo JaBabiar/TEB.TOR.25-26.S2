baf79b1130b05bbf9ba7b8239a2d6854f659688b
JaBabiar <bromomento2137@gmail.com>
2026-04-10 08:47:07 +0200

    poprawa listy sprzetu msi

--- 3.tp.cisco/Tydzien 6/main.py
+++ 3.tp.cisco/Tydzien 6/main.py
@@ -10,18 +10,20 @@ testowaLista = ("jedne", "dwa", "tri")
 print(list(map(lambda x: "numer: "+x, testowaLista)))
 
 ## Filter - produkty w przedziale cenowym 
-sprzet = msi_sprzet = {
-    "MSI Vigor GK30": {"typ": "klawiatura", "cena": 180},
-    "MSI Vigor GK50 Low Profile": {"typ": "klawiatura", "cena": 350},
-    "MSI Vigor GK50 Elite": {"typ": "klawiatura", "cena": 420},
-    "MSI Clutch GM08": {"typ": "mysz", "cena": 70},
-    "MSI Clutch GM11": {"typ": "mysz", "cena": 90},
-    "MSI Clutch GM20 Elite": {"typ": "mysz", "cena": 150},
-    "MSI Immerse GH20": {"typ": "słuchawki", "cena": 120},
-    "MSI Immerse GH30 V2": {"typ": "słuchawki", "cena": 115},
-    "MSI Agility GD20": {"typ": "podkładka", "cena": 45},
-    "MSI Agility GD60": {"typ": "podkładka", "cena": 150}
-}
+msi_sprzet = [
+    {"nazwa": "MSI Vigor GK30", "typ": "klawiatura", "cena": 180},
+    {"nazwa": "MSI Vigor GK50 Low Profile", "typ": "klawiatura", "cena": 350},
+    {"nazwa": "MSI Vigor GK50 Elite", "typ": "klawiatura", "cena": 420},
+    {"nazwa": "MSI Clutch GM08", "typ": "mysz", "cena": 70},
+    {"nazwa": "MSI Clutch GM11", "typ": "mysz", "cena": 90},
+    {"nazwa": "MSI Clutch GM20 Elite", "typ": "mysz", "cena": 150},
+    {"nazwa": "MSI Immerse GH20", "typ": "słuchawki", "cena": 120},
+    {"nazwa": "MSI Immerse GH30 V2", "typ": "słuchawki", "cena": 115},
+    {"nazwa": "MSI Agility GD20", "typ": "podkładka", "cena": 45},
+    {"nazwa": "MSI Agility GD60", "typ": "podkładka", "cena": 150}
+]
 minimalna = input("Podaj Cene minimalną: ")
 maksymalna = input("Podaj Cene Maksymalną: ")
+filtrowana = list(filter(lambda x: int(minimalna) <= x["cena"] <= int(maksymalna), msi_sprzet ))
+print(filtrowana)
 
